69d49ae70e0d35466e87b0a4c84b92e948e95650
Rohit kumar <82631765+rohitkumar-7@users.noreply.github.com>
2025-04-28 13:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+	if (!str) return false;
+  
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(str);
 }
 
 // Do not change the code below.
